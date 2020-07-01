b29390d83f75c505435b357d5c8d398b7009455d
GURRAM, KALYAN (kg523e) <kg523e@att.com>
2020-06-30 21:18:48 -0400

    [kg523e] Submitting new changes for javaproject

---
+++ src/com/att/samplejavaproject/JenkinsTest.java
@@ -0,0 +1,11 @@
+package com.att.samplejavaproject;
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			System.out.println("Print in jenkins : "+i);
+		}
+	}
+
+}
