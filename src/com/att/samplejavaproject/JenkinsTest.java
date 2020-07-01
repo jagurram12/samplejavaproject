5e9abcea00f30b50724175b8a4801bd7f6dea1b5
GURRAM, KALYAN (kg523e) <kg523e@att.com>
2020-06-30 21:30:42 -0400

    second commit from sample java project

--- src/com/att/samplejavaproject/JenkinsTest.java
+++ src/com/att/samplejavaproject/JenkinsTest.java
@@ -4,6 +4,7 @@ public class JenkinsTest {
 
 	public static void main(String[] args) {
 		for(int i=0;i<10;i++) {
+			System.out.println("Testing branches ");
 			System.out.println("Print in jenkins : "+i);
 		}
 	}
